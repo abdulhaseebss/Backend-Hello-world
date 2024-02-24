48b12b910cc8c8d49723c97c57e515feeaa7b01a
abdulhaseebss <m.saleemhaseeb@gmail.com>
2024-02-24 22:23:18 +0500

    Backend Todo

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
-const express = require('express');
-
-require('dotenv').config()
-
+const express = require('express')
+var cors = require('cors')
 const app = express()
+const port = 3000
+
+
+//middleware
+app.use(express.json());
+app.use(cors())
+
+
+const arr = [];
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Hello World</h1>`)
-})
-
-app.get('/home' , (req , res)=>{
-    res.send(`<h1>Abdul Haseeb</h1>`)
-})
-
-app.get('/signup' , (req , res)=>{
-    res.send(`<h1>signup page</h1>`)
+    res.send('Hello World!')
 })
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
+//get all users
+app.get('/api/v1/users', (req, res) => {
+    res.send(arr)
+})
+
+//add user
+app.post('/api/v1/users', (req, res) => {
+    const { title } = req.body;
+    arr.push({
+        title: title,
+        id: Date.now(),
+    })
+    res.send('user added successfully');
+})
+
+//delete user
+app.delete('/api/v1/users/:id', (req, res) => {
+    const { id } = req.params
+    const index = arr.findIndex((user) => user.id === Number(id));
+    if (index === -1) {
+        res.send('user not found');
+        return
+    }
+    arr.splice(index, 1);
+    res.send('user deleted');
+})
+
+//edit user
+app.put('/api/v1/users/:id', (req, res) => {
+    const { title } = req.body;
+    const { id } = req.params;
+    const index = arr.findIndex((user) => user.id === Number(id));
+    if (index === -1) {
+        res.send('user not found');
+        return
+    }
+    arr[index].title = title
+    res.send('user edited successfully')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 })
